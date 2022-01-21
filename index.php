3e6559193d637bd1d4d2c99280df3dce418ee6a1
alx-nder <tyreekalx@gmail.com>
2022-01-21 13:18:15 -0500

    encoded get variable

--- index.php
+++ index.php
@@ -37,20 +37,9 @@
     
         <?php
         if (isset($_GET['chat'])) {
-          $chattinglink = "http://localhost/virtualtourwebsite/chatbot1/chatbot1.py?". $_GET['chat'];
-        
-
-          function file_get_contents_curl($url) {
-            $ch = curl_init();
-            curl_setopt($ch, CURLOPT_HEADER, 0);
-            curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); //Set curl to return the data instead of printing it to the browser.
-            curl_setopt($ch, CURLOPT_URL, $url);
-            $data = curl_exec($ch);
-            curl_close($ch);
-            return $data;
-          }
-
-          $chatter=file_get_contents_curl( $chattinglink);
+          $chattinglink = "http://localhost/virtualtourwebsite/chatbot1/chatbot1.py?". urlencode($_GET['chat']);
+      
+          $chatter=file_get_contents($chattinglink);
           
           
           echo $chatter;
