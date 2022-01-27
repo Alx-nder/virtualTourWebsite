a34b5fb3500d287c4806f5c83ac79f4f1cb6ae87
alx-nder <tyreekalx@gmail.com>
2022-01-26 21:53:40 -0500

    working zoom

--- zoomapi/makemeeting.py
+++ zoomapi/makemeeting.py
@@ -4,8 +4,6 @@ import requests
 import json
 import datetime
 
-
-
 # Enter your API key and your API secret
 API_KEY = 'P2VgBjzBSGmWExeDMPgHiQ'
 API_SEC = 'LwbEhSMV8rvITHDhBOoGc6E0Yu5Ut2F42Luw'
@@ -21,7 +19,7 @@ headers={
 	"typ": "JWT"
 	}
 payload ={
-	"iss": "API_KEY",
+	"iss": API_KEY,
 	"exp": expiringfrmt
 }
 
@@ -30,92 +28,15 @@ encodedjwt= jwt.encode(payload,API_SEC,algorithm="HS256")
 email= "info.virttour@gmail.com"
 
 url='https://api.zoom.us/v2/users/{}/meetings'.format(email)
-obj={"topic":"Live agent","password":"12345",
-"type": 2,
-                  "start_time": "2019-06-14T10: 21: 57",
-                  "duration": "45",
-                  "timezone": "Europe/Madrid",
-"recurrence": {"type": 1,
-"repeat_interval": 1},
-"settings": {"host_video": "true",
-	"participant_video": "true",
-	"join_before_host": "False",
-	"mute_upon_entry": "False",
-	"watermark": "true",
-	"audio": "voip",
-	"auto_recording": "cloud"
-	}
-}
-header = {"authorization":"Bearer{}".format(encodedjwt)}
+header = {"authorization":"Bearer {}".format(encodedjwt)}
+date = datetime.datetime(2022,1,26,23,30).strftime("%Y-%M-%dT%H:%M:%SZ")
+
+obj={"topic":"Live agent","starttime":date, "duration":30, "password":"12345"}
+
 createmeeting = requests.post(url,json=obj, headers= header)
-#print (createmeeting.text)
-y = json.loads(createmeeting.text)
-print(y)
-#join_URL = y["join_url"]
-#meetingPassword = y["password"]
 
-#print(f'\n here is your zoom meeting link {join_URL} and your \password: "{meetingPassword}"\n')
+file = json.loads(createmeeting.text)
+join_URL = file["join_url"]
+meetingPassword = file["password"]
 
-
-# def generateToken():
-# 	token = jwt.encode(
-
-# 		# Create a payload of the token containing
-# 		# API Key & expiration time
-# 		{'iss': API_KEY, 'exp': time() + 5000},
-
-# 		# Secret used to generate token signature
-# 		API_SEC,
-
-# 		# Specify the hashing alg
-# 		algorithm='HS256'
-# 	)
-# 	return token.decode('utf-8')
-
-
-# # create json data for post requests
-# meetingdetails = {"topic": "The title of your zoom meeting",
-# 				"type": 2,
-# 				"start_time": "2019-06-14T10: 21: 57",
-# 				"duration": "45",
-# 				"timezone": "Europe/Madrid",
-# 				"agenda": "test",
-
-# 				"recurrence": {"type": 1,
-# 								"repeat_interval": 1
-# 								},
-# 				"settings": {"host_video": "true",
-# 							"participant_video": "true",
-# 							"join_before_host": "False",
-# 							"mute_upon_entry": "False",
-# 							"watermark": "true",
-# 							"audio": "voip",
-# 							"auto_recording": "cloud"
-# 							}
-# 				}
-
-# # send a request with headers including
-# # a token and meeting details
-
-
-# def createMeeting():
-# 	headers = {'authorization': 'Bearer ' + generateToken(),
-# 			'content-type': 'application/json'}
-# 	r = requests.post(
-# 		f'https://api.zoom.us/v2/users/me/meetings',
-# 		headers=headers, data=json.dumps(meetingdetails))
-
-# 	print("\n creating zoom meeting ... \n")
-# 	# print(r.text)
-# 	# converting the output into json and extracting the details
-# 	y = json.loads(r.text)
-# 	join_URL = y["join_url"]
-# 	meetingPassword = y["password"]
-
-# 	print(
-# 		f'\n here is your zoom meeting link {join_URL} and your \
-# 		password: "{meetingPassword}"\n')
-
-
-# # run the create meeting function
-# createMeeting()
+print(f'\n here is your zoom meeting link {join_URL} and your \password: "{meetingPassword}"\n')
