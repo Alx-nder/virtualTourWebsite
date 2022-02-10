dd10b0b8244ea48047e64ee8413eb2f845096d36
alx-nder <tyreekalx@gmail.com>
2022-02-10 00:39:21 -0500

    fixing for reccomend output

--- base.php
+++ base.php
@@ -111,8 +111,9 @@
           echo  $data['price'];
           echo $data['add'];
         }
-    ?>;'
-      nexrec.nex_nested_div_text.nex_nested_div_text.innerHTML=data['description'];
+    ?>;';
+      rec_des=des;
+      console.log("des");
     </script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 
--- reccomend.js
+++ reccomend.js
@@ -1,4 +1,6 @@
-
+var rec_price;
+var rec_add;
+var rec_des;
 
 var nexrec= document.getElementById("nexrec");
 nexrec.addEventListener("click", function(){
@@ -23,7 +25,8 @@ nexrec.addEventListener("click", function(){
     var nex_nested_div_text = document.createElement("p");   
     nex_nested_div_text.setAttribute("class", "card-text");
     nex_nested_div.appendChild(nex_nested_div_text);
-    nex_nested_div_text.innerHTML="This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.";
+    rec_des = nex_nested_div_text.innerHTML ;
+    // rec_des="This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.";
 
     var nex_nested_footer = document.createElement("div");   
     nex_nested_footer.setAttribute("class", "card-footer");
