58d1613161742f564f8291b4a04f2e2f38b25642
alx-nder <tyreekalx@gmail.com>
2022-02-25 01:30:14 -0500

    frontend work

--- base.php
+++ base.php
@@ -15,61 +15,72 @@
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" >
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="listings.css">
     
 </head>
 <body>
-<div class="card-group" id="csec">
-      <div class="card">
-        <!-- <img src="..." class="card-img-top" alt="..."> -->
-        <div class="card-body">
-          <h5 class="card-title">Card title</h5>
-          <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-        </div>
-        <div class="card-footer">
-          <small class="text-muted">Last updated 3 mins ago</small>
-        </div>
-      </div>
-      <div class="card">
-        <!-- <img src="..." class="card-img-top" alt="..."> -->
-        <div class="card-body">
-          <h5 class="card-title">Card title</h5>
-          <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
-        </div>
-        <div class="card-footer">
-          <small class="text-muted">Last updated 3 mins ago</small>
+  <div id="for_rec" class="container">
+    <div class=" row row-cols-1 row-cols-md-3 g-4" id="csec">
+      <div class="col">
+        <div class="card h-100">
+          <img src="https://images.pexels.com/photos/3958954/pexels-photo-3958954.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+          </div>
+          <div class="card-footer">
+            <small class="text-muted">Last updated 3 mins ago</small>
+          </div>
         </div>
       </div>
-      <div class="card">
-        <!-- <img src="..." class="card-img-top" alt="..."> -->
-        <div class="card-body">
-          <h5 class="card-title">Card title</h5>
-          <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+      <div class="col">
+        <div class="card h-100">
+          <img src="https://images.pexels.com/photos/3958954/pexels-photo-3958954.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+          </div>
+          <div class="card-footer">
+            <small class="text-muted">Last updated 3 mins ago</small>
+          </div>
         </div>
-        <div class="card-footer">
-          <small class="text-muted">Last updated 3 mins ago</small>
+      </div>    
+      <div class="col">
+        <div class="card h-100">
+          <img src="https://images.pexels.com/photos/3958954/pexels-photo-3958954.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+          </div>
+          <div class="card-footer">
+            <small class="text-muted">Last updated 3 mins ago</small>
+          </div>
         </div>
       </div>
     </div>
+  </div>
+    <a href=""> next</a>
+<h1 id="nexrec" >next</h1>
 
-    <a id="nexrec" href="#"> next</a>
+<!-- The Modal -->
 
     <!-- search form -->
 
-<form action="base.php" method="post">
+<!-- <form action="base.php" method="post">
   <div >
       <input type="text" name="price"   placeholder="price">
     </div>
 
     <div class="form-group">
         <button class="btn mx-auto  btn-danger" type= "submit">search</button>
-    </div>
+    </div> -->
 <!-- search fuction -->
 <?php
     if (isset($_POST['price'])) {
@@ -96,10 +107,14 @@
       }
     ?> 
 
+
+
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script  type = "text/javascript" src="reccomend.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 
+
+    
 </body>
 </html>
 
--- reccomend.js
+++ reccomend.js
@@ -1,17 +1,51 @@
+// Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var list_img = document.getElementsByClassName('list_img');
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+
+for (var i = 0, len = list_img.length; i < len; ++i) {
+    list_img[i].addEventListener("click", function () {
+        
+        modal.style.display = "block";
+        modalImg.src = this.src;
+        // captionText.innerHTML = this.alt;
+    }, false);
+}
+
+
+
+// // Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() { 
+  modal.style.display = "none";
+}
+
 var nexrec= document.getElementById("nexrec");
    nexrec.addEventListener("click", function(){
 $.ajax({
     url:"http://localhost/recommendAlgo/epsilon1.py",
     type:"POST",
     success:  function(resp){
+        
         resp=JSON.parse(resp);
+
+        var outer_div = document.createElement("div");
+        outer_div.setAttribute("class","col"); 
+        
+
         var nex = document.createElement("div");
-        nex.setAttribute("class","card"); 
+        nex.setAttribute("class","card h-100 bg-light"); 
+        outer_div.appendChild(nex);
         
         var nex_nested = document.createElement("img");   
-        nex_nested.setAttribute("src", "...");
-        nex_nested.setAttribute("class", "card-img-top");
-        nex_nested.setAttribute("alt","...");
+        nex_nested.setAttribute("src", resp[5]);
+        nex_nested.setAttribute("class", "card-img-top list_img");
+        nex_nested.setAttribute("style", "max-width:100%; height:auto; object-fit:contain;");
         nex.appendChild(nex_nested);
 
         var nex_nested_div = document.createElement("div");   
@@ -21,7 +55,7 @@ $.ajax({
         var nex_nested_div_title = document.createElement("h5");   
         nex_nested_div_title.setAttribute("class", "card-title");
         nex_nested_div.appendChild(nex_nested_div_title);
-        nex_nested_div_title.innerHTML="Card NEWtitle";
+        nex_nested_div_title.innerHTML=""+ resp[1] + "<br>"+ new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(resp[4]);
 
         var nex_nested_div_text = document.createElement("p");   
         nex_nested_div_text.setAttribute("class", "card-text");
@@ -35,12 +69,13 @@ $.ajax({
         var nex_nested_footer_text = document.createElement("small");   
         nex_nested_footer_text.setAttribute("class", "text-muted");
         nex_nested_footer.appendChild(nex_nested_footer_text);
-        nex_nested_footer_text.innerHTML="Last updated 3 mins ago";
+        nex_nested_footer_text.innerHTML="Posted: "+ resp[2];
         
-        document.getElementById("csec").appendChild(nex);
+        document.getElementById("csec").appendChild(outer_div);
     
-        var prev = document.getElementsByClassName('card')[0];
-        document.getElementById("csec").removeChild(prev);
+        // var prev = document.getElementsByClassName('card')[0];
+        // document.getElementById("csec").removeChild(prev);
+       
     }
 
 })
