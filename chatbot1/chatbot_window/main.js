4bd1f3a6005f950d20c6ffe81a6652a875f1a413
alx-nder <tyreekalx@gmail.com>
2022-02-27 23:05:34 -0500

    js for messenger

---
+++ chatbot1/chatbot_window/main.js
@@ -0,0 +1,9 @@
+const picker = new EmojiButton();
+const trigger = document.querySelector('.trigger');
+
+picker.on('emoji', selection => {
+  // `selection` object has an `emoji` property
+  // containing the selected emoji
+});
+
+trigger.addEventListener('click', () => picker.togglePicker(trigger));
