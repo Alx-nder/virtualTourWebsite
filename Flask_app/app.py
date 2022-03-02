2e62a068ebc2b21d744c370b8dbdfbe50cd0d004
alx-nder <tyreekalx@gmail.com>
2022-03-01 22:49:23 -0500

    pyaudio work

--- Flask_app/app.py
+++ Flask_app/app.py
@@ -1,3 +1,4 @@
+'''
 from distutils.log import debug
 
 from flask import Flask,render_template, request
@@ -11,3 +12,98 @@ def home():
 
 if __name__=="__main__":
     app.run(debug= True, port=5001)
+
+'''
+
+# ######### https://stackoverflow.com/questions/51079338/audio-livestreaming-with-python-flask
+
+from flask import Flask, Response,render_template
+import pyaudio
+import speech_recognition as sr
+
+app = Flask(__name__)
+
+
+FORMAT = pyaudio.paInt16
+CHANNELS = 2
+RATE = 44100
+CHUNK = 1024
+RECORD_SECONDS = 5
+
+
+audio1 = pyaudio.PyAudio()
+
+
+
+def genHeader(sampleRate, bitsPerSample, channels):
+    datasize = 2000*10**6
+    o = bytes("RIFF",'ascii')                                               # (4byte) Marks file as RIFF
+    o += (datasize + 36).to_bytes(4,'little')                               # (4byte) File size in bytes excluding this and RIFF marker
+    o += bytes("WAVE",'ascii')                                              # (4byte) File type
+    o += bytes("fmt ",'ascii')                                              # (4byte) Format Chunk Marker
+    o += (16).to_bytes(4,'little')                                          # (4byte) Length of above format data
+    o += (1).to_bytes(2,'little')                                           # (2byte) Format type (1 - PCM)
+    o += (channels).to_bytes(2,'little')                                    # (2byte)
+    o += (sampleRate).to_bytes(4,'little')                                  # (4byte)
+    o += (sampleRate * channels * bitsPerSample // 8).to_bytes(4,'little')  # (4byte)
+    o += (channels * bitsPerSample // 8).to_bytes(2,'little')               # (2byte)
+    o += (bitsPerSample).to_bytes(2,'little')                               # (2byte)
+    o += bytes("data",'ascii')                                              # (4byte) Data Chunk Marker
+    o += (datasize).to_bytes(4,'little')                                    # (4byte) Data size in bytes
+    return o
+
+@app.route('/audio')
+def audio():
+    # start Recording
+    # obtain audio from the microphone
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Say something!")
+        audio = r.listen(source)
+
+
+    # recognize speech using Google Speech Recognition
+    try:
+        # the default API (no keys needed)
+        speech=r.recognize_google(audio)
+        print(speech)
+        return speech
+
+    except sr.UnknownValueError:
+        print("Google Speech Recognition could not understand audio")
+    except sr.RequestError as e:
+        print("Could not request results from Google Speech Recognition service; {0}".format(e))
+
+    # def sound():
+
+    #     CHUNK = 1024
+    #     sampleRate = 44100
+    #     bitsPerSample = 16
+    #     channels = 2
+    #     wav_header = genHeader(sampleRate, bitsPerSample, channels)
+
+    #     stream = audio1.open(format=FORMAT, channels=CHANNELS,
+    #                     rate=RATE, input=True,input_device_index=1,
+    #                     frames_per_buffer=CHUNK)
+    #     print("recording...")
+    #     #frames = []
+    #     first_run = True
+    #     while True:
+    #        if first_run:
+    #            data = wav_header + stream.read(CHUNK)
+    #            first_run = False
+    #        else:
+    #            data = stream.read(CHUNK)
+    #        yield(data)
+
+    # return Response(sound())
+
+
+@app.route('/')
+def index():
+    """Video streaming home page."""
+    return render_template('index.html')
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', debug=True, threaded=True,port=5000)
