25cbb23cd0d02a7f1b334d6b681050d90cc96355
alx-nder <tyreekalx@gmail.com>
2022-03-12 23:27:08 -0500

    linear regression innit

---
+++ seller/estimation.py
@@ -0,0 +1,32 @@
+#!C:\Program Files\Python310\python.exe
+print("content-type: text/html;\n\n" )
+
+import mysql.connector as connection
+import pandas as pd
+import numpy as np
+from sklearn.linear_model import LinearRegression
+
+# connecting to database
+db=connection.connect(
+    host="localhost",
+    user="root",
+    database="virttour"
+) 
+
+query="select * from listings"
+
+db_data= pd.read_sql(query,db)
+print(db_data)
+
+predictors=["living_space","bathrooms","building_class","land"]
+outcome='price'
+
+# print(predictors)
+
+model = LinearRegression()
+model.fit(db_data[predictors],db_data[outcome])
+
+print(f'intercept: {model.intercept_:.3f}')
+print("Coefficients: ")
+for name, coef in zip(predictors, model.coef_):
+    print(f" {name} : {coef}")
