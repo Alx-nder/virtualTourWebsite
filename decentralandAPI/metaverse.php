76a9bd2180e1d3b5fbdc9a9f21e9e045f16b2525
alx-nder <tyreekalx@gmail.com>
2022-03-17 17:03:29 -0500

    changeing metaverse landing page

--- decentralandAPI/metaverse.php
+++ decentralandAPI/metaverse.php
@@ -25,29 +25,33 @@
     <link rel="stylesheet" href="http://localhost/virtualTourWebsite/listings.css">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" rel="stylesheet" media="all">
   </head>
-<body style="background-color: #10051b!important;" class="container-fluid text-light">
+<body style="background-color: #10051b!important;   background-size: 100% 100%;   object-fit: cover;
+" class="container-fluid text-light">
     <!-- navbar -->
-  <nav class="navbar navbar-expand-lg navbar-dark ">
-    <div class="container-fluid">
-      <a class="navbar-brand h3" href="http://localhost/virtualTourWebsite/logout.php">VtZ</a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse justify-content-center navbar-collapse" id="navbarTogglerDemo02">
-        <ul class="navbar-nav  mb-2 justify-content-center mb-lg-0">
-          <li class="nav-item">
-            <a class="nav-link" href="http://localhost/virtualTourWebsite/listings.php">Buy</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="http://localhost/virtualTourWebsite/seller/sell.php">Sell</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#"  >METAverse</a>
-          </li>
-        </ul>      
+    <div class="min-vh-100" style="background-image: url('http://localhost/virtualTourWebsite/milad-fakurian-wNsHBf_bTBo-unsplash.jpg');">
+      <nav class="navbar navbar-expand-lg navbar-dark ">
+      <div class="container-fluid">
+        <a class="navbar-brand h3" href="http://localhost/virtualTourWebsite/logout.php">VtZ</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse justify-content-center navbar-collapse" id="navbarTogglerDemo02">
+          <ul class="navbar-nav  mb-2 justify-content-center mb-lg-0">
+            <li class="nav-item">
+              <a class="nav-link" href="http://localhost/virtualTourWebsite/listings.php">Buy</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="http://localhost/virtualTourWebsite/seller/sell.php">Sell</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="#"  >METAverse</a>
+            </li>
+          </ul>      
+        </div>
       </div>
+    </nav>
     </div>
-  </nav>
+  
   
 <input style="display: none;" type="text" name="user_session" value=<?php echo $_SESSION['email'];?>>  
 
