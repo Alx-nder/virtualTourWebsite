ef55f233b45fe56dce36736f32c453abfe59df66
alx-nder <tyreekalx@gmail.com>
2022-03-16 20:32:14 -0500

    video added to landing page

--- listings.php
+++ listings.php
@@ -24,29 +24,41 @@ if(!$con)
     <link rel="stylesheet" href="listings.css">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" rel="stylesheet" media="all">
   </head>
-<body class="container-fluid">
+<body  >
+  <div class="min-vh-100">
+    <video autoplay muted loop poster="https://images.unsplash.com/photo-1488707872600-5507977fe355?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80">
+      <source type="video/mp4" src="housetourvideo.mp4">
+      <!-- pic credit https://images.unsplash.com/photo-1488707872600-5507977fe355?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80 -->
+      <!-- vid credit https://www.youtube.com/watch?v=An0SK0n7Xeo -->
+      </video>
     <!-- navbar -->
-  <nav class="navbar navbar-expand-lg navbar-light ">
-    <div class="container-fluid">
-      <a class="navbar-brand h3" href="logout.php">VtZ</a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse justify-content-center navbar-collapse" id="navbarTogglerDemo02">
-        <ul class="navbar-nav  mb-2 justify-content-center mb-lg-0">
-          <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#">Buy</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="seller\sell.php">Sell</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="decentralandAPI\metaverse.php"  >METAverse</a>
-          </li>
-        </ul>      
+    <nav class="navbar navbar-expand-lg navbar-light ">
+      <div class="container-fluid">
+        <a class="navbar-brand h3" href="logout.php">VtZ</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse justify-content-center navbar-collapse" id="navbarTogglerDemo02">
+          <ul class="navbar-nav  mb-2 justify-content-center mb-lg-0">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="#">Buy</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="seller\sell.php">Sell</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="decentralandAPI\metaverse.php"  >METAverse</a>
+            </li>
+          </ul>      
+        </div>
       </div>
-    </div>
-  </nav>
+    </nav>
+  </div>
+<div >
+  
+</div>
+
+  
   <h5 class="card-title" id="user_id" ><?php echo $_SESSION['email'];?></h5>
                 
   <!-- chatbot -->
@@ -79,6 +91,9 @@ if(!$con)
     <embed class="modal-content" id="for_tour" src="" type="">
   </div>
  
+  <div class="min-vh-100" style="background-color: #2672a4;">
+
+ </div>
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
   <script  type = "text/javascript" src="reccomend.js"></script>
   <script src="chatbot.js"></script>
