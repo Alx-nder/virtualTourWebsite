7b5b7bf459c4db02c704df282855a11738848abd
alx-nder <tyreekalx@gmail.com>
2022-03-17 20:58:34 -0500

    regression work

--- seller/estimation.py
+++ seller/estimation.py
@@ -16,17 +16,19 @@ db=connection.connect(
 query="select * from listings"
 
 db_data= pd.read_sql(query,db)
-print(db_data['price'])
+# print(db_data['price'])
 
-predictors=["living_space","bathrooms","building_class","land"]
+predictors=["living_space","bathrooms","bedrooms","building_class","land", "age"]
 outcome='price'
+
+# x=pd.get_dummies(db_data[predictors], drop_first=True)
 
 # print(predictors)
 
-# model = LinearRegression()
-# model.fit(db_data[predictors],db_data[outcome])
+model = LinearRegression()
+model.fit(db_data[predictors],db_data[outcome])
 
-# print(f'intercept: {model.intercept_:.3f}')
-# print("Coefficients: ")
-# for name, coef in zip(predictors, model.coef_):
-#     print(f" {name} : {coef}")
+print(f'intercept: {model.intercept_:.3f}')
+print("Coefficients: ")
+for name, coef in zip(db_data[predictors], model.coef_):
+    print(f" {name} : {coef}")
