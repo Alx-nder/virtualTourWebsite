0d4589ed53a2438778a948922d29944d04ad05f0
alx-nder <tyreekalx@gmail.com>
2022-04-29 12:10:57 -0500

    listings edit

--- web/seller/sell.php
+++ web/seller/sell.php
@@ -20,25 +20,37 @@
 <!DOCTYPE html>
 <html lang="en"  >
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" rel="stylesheet" media="all">
-  </head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <!-- Bootstrap CSS -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+  <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" rel="stylesheet" media="all">
+  <link rel="stylesheet" href="http://localhost/virtualTourWebsite/web/index/listings.css">
+  <style>
+    .border-bottom{
+      border-bottom:1px solid #6c757d!important;
+    }   
+    ::placeholder {
+  color: #343a40  !important;;
+  opacity: 1; /* Firefox */
+  text-align: left;
+}
+  </style>
+</head>
+
 <body style="background-color: #7f9497!important;" class="container-fluid text-dark">
     <!-- navbar -->
   <nav class="navbar pt-3 navbar-expand-lg navbar-light ">
     <div class="container-fluid">
-    <a href="http://localhost/virtualTourWebsite/listings.php"><img class="mx-3" srcset="http://localhost/virtualTourWebsite/web/images/logo.png 3x" alt=""></a>
+      <a href="http://localhost/virtualTourWebsite/web/index/listings.php"><img class="mx-3" srcset="http://localhost/virtualTourWebsite/web/images/logo.png 3x" alt=""></a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse justify-content-center navbar-collapse" id="navbarTogglerDemo02">
         <ul class="navbar-nav  mb-2 justify-content-center mb-lg-0">
           <li class="nav-item">
-            <a class="nav-link" href="http://localhost/virtualTourWebsite/listings.php">Buy</a>
+            <a class="nav-link" href="http://localhost/virtualTourWebsite/web/index/listings.php">Buy</a>
           </li>
           <li class="nav-item">
             <a class="nav-link active" aria-current="page" href="#">Sell</a>
@@ -54,11 +66,13 @@
     </div>
   </nav>
 
-  <div class="container">
-  <div class="row row-cols-1 row-cols-md-3 g-4">
+  <div class="container ">
+  <hr class=" my-4 ">
+  <h3 class='pb-3'>Your Listings</h3>
+    <div class="list-group-horizontal list-group position-relative">
 
-  <?php
-    echo"These are your listings";
+    <div class="row row-cols-1 row-cols-md-3 g-4">
+      <?php
         if (isset($_SESSION['email'])){
         $user=$_SESSION['email'];
         $sql = "SELECT * FROM listings where posted_by='$user' ORDER BY listings_interaction desc";
@@ -66,90 +80,117 @@
 
         // Associative array
         while($row = $result -> fetch_assoc()){
-          echo"<div class='col'>  <div class='card h-100 bg-light'><img src=",$row['image_src']," class='list_img my_img rounded my-2 mx-2' style='max-width:100%; height:auto; object-fit:contain;' alt='http://localhost/krpano-1.20.11/viewer/krpano.html?xml=examples/interactive-area/interactive-area.xml'><div class='card-body'><h5 class='card-title'>",$row['house_location'],"</h5><i class='bi bi-cash-coin'></i><h5 class='card-title'>",$row['price'],"</h5><p class='card-text fs-6'>Total acres of land: ",$row['land'],"<br>Total acres of living space: ",$row['living_space'],"<br>No. of Bedrooms: ",$row['bedrooms'],"<br>No. of Bathrooms: ",$row['bathrooms'],"<br>Built/renovated: ",$row['age']," years ago</p></div><div class='card-footer'><small class='text-muted'>Contact: ",$row['posted_by'],"</small></div></div></div>";
+          echo"
+          <div class='col'>  
+            <div class='card h-100 bg-light'>
+              <img src=",$row['image_src']," class='list_img my_img rounded my-2 mx-2' style='max-width:100%; height:auto; object-fit:contain;' alt='http://localhost/krpano-1.20.11/viewer/krpano.html?xml=examples/interactive-area/interactive-area.xml'>
+            
+                <div class='card-body'>
+                  <h5 class='card-title'>",$row['house_location'],"</h5>
+                  <h5 class='card-title'>",$row['price'],"</h5>
+                  <p class='card-text fs-6'>Total acres of land: ",$row['land'],"
+                    <br>Total acres of living space: ",$row['living_space'],"
+                    <br>No. of Bedrooms: ",$row['bedrooms'],"
+                    <br>No. of Bathrooms: ",$row['bathrooms'],"
+                    <br>Built/renovated: ",$row['age']," years ago
+                  </p>
+                </div>
+              <div class='card-footer'>
+                <small class='text-muted'>Contact: ",$row['posted_by'],"</small>
+              </div>
+            </div>
+          </div>";
         }
         // Free result set
         $result -> free_result();
-      }
+        }
       ?>
+    </div>
+    </div>
+    <hr class=" my-4 ">
   </div>
-  </div>
+ 
+  <main>
 
-    <main>
     <form  action="upload.php" method="POST" enctype="multipart/form-data">
-
-    <div class="container py-4 mt-5 px-4" style="background-color: #ffffff57!important;">
-        <div class=" rounded my-3 row align-self-center">    
-          <div class="card col bg-transparent border-0">
-           <div>
-             <img srcset="https://cdn.pixabay.com/photo/2018/11/13/21/43/instagram-3814049_960_720.png 5x"  alt="user">
-           </div> 
+      <div class="container py-4 mt-5 px-4" style="background-color: #ffffff57!important;">
+        <div class=" rounded mt-5 row  align-self-center">
+          <!-- user pic -->
+          <div class=" col text-center bg-transparent border-0">
+            <h3 class="py-3">Upload a listing </h3>
+            <div>
+              <img srcset="https://cdn.pixabay.com/photo/2018/11/13/21/43/instagram-3814049_960_720.png 5x"  alt="user">
+            </div> 
             <!-- pic from https://pixabay.com/vectors/instagram-insta-user-instagram-icon-3814049/ -->
             <div class="card-body">
-                <h5 class="card-title" id="user_id" >Hi <?php echo $_SESSION['email'];?>!</h5>
+              <h5 class="card-title" id="user_id" >Hi <?php echo $_SESSION['email'];?>!</h5>
             </div>
             <div class="card-body">
-                <p class="card-title" id="user_id" >Use this page to upload a listing you plan to sell. You can also use the estimate button to see what the prices are like based on the details you provide.</p>
+              <p class="card-title" id="user_id" >Use this page to upload a listing you plan to sell. You can also use the estimate button to see what the prices are like based on the details you provide.</p>
             </div>
           </div>
           
-          <div class="col">
-              <div class="input-group mb-3">
-                <input type="text" class="form-control house_details" name="living_space" placeholder="acres of living space" required>
-              </div>
-              <div class="input-group mb-3">
-                <input type="text" class="form-control house_details" name="bathrooms" placeholder="no. of bathrooms"required>
-              </div>
-              <div class="input-group mb-3">
-                <input type="text" class="form-control house_details" name="bedrooms" placeholder="no. of bedrooms"required>
-              </div>
-              <div class="input-group mb-3">
-                <select class="form-select" name="building_class" aria-label="Default select example" required>
-                  <option selected>Building class</option>
-                  <option value="1">board house flat</option>
-                  <option value="3">concrete flat</option>
-                  <option value="5">concrete complex (multi-story)</option>
-                  <option value="7">villa-type</option>
-                  <option value="9">mansion</option>
-                </select>  
-              </div>
+          <div class="col pt-5 mx-3">
+            <div class="input-group mb-3">
+              <input type="text"  class="border-bottom form-control bg-transparent border-0 rounded-0 house_details" name="living_space" placeholder="acres of living space" required>
+            </div>
+            <div class="input-group mb-3">
+              <input type="text" class="form-control border-bottom bg-transparent border-0 rounded-0 house_details" name="bathrooms" placeholder="no. of bathrooms"required>
+            </div>
+            <div class="input-group mb-3">
+              <input type="text" class="form-control house_details bg-transparent border-0 rounded-0 border-bottom" name="bedrooms" placeholder="no. of bedrooms"required>
+            </div>
+            <div class="input-group mb-3">
+              <select class="form-select border-0 border-bottom  rounded-0 bg-transparent" name="building_class" aria-label="Default select example" required>
+                <option selected>Building class</option>
+                <option value="1">board house flat</option>
+                <option value="3">concrete flat</option>
+                <option value="5">concrete complex (multi-story)</option>
+                <option value="7">villa-type</option>
+                <option value="9">mansion</option>
+              </select>  
+            </div>
 
-              <div class="input-group mb-3">
-                <input type="text" class="form-control house_details" name="land" placeholder="acres of land"required>
-              </div>
-              <div class="input-group mb-3">
-                <input type="text" class="form-control house_details" name="age" placeholder="age in years"required>
-              </div>
+            <div class="input-group mb-3">
+              <input type="text" class="form-control border-0 border-bottom  rounded-0 bg-transparent house_details" name="land" placeholder="acres of land"required>
+            </div>
+            <div class="input-group mb-3">
+              <input type="text" class="form-control border-0 border-bottom  rounded-0 bg-transparent house_details" name="age" placeholder="age in years"required>
+            </div>
+            <div>
               <ul class="list-group list-group-horizontal">
                 <a class="btn border bg-transparent submit">Estimate</a>
-                <input type="text"  id="the_estimate" disabled placeholder="">
+                <input type="text" class="form-control"  id="the_estimate" disabled placeholder="">
               </ul>
             </div>
-
-            <div class="col">
-            <div class="input-group mb-3">
-                <input type="text" class="form-control house_details" name="description" placeholder="Extra deatil/ Description">
-              </div>
-              <div class="input-group mb-3">
-                <input type="text" class="form-control house_details" name="price" placeholder="Asking price"required>
-              </div>
-              <div class="input-group mb-3">
-                <input type="text" class="form-control house_details" name="house_location" placeholder="location"required>
-              </div>
-              <div class="input-group pt-3 mb-2">
-                 <label >Upload pictute of the listing</label>
-              </div>
-              <div class="input-group mb-3">
-                <input type="file" class="form-control house_details" name="image" required>
-              </div>
-              <button type="submit" class="btn border bg-transparent submit" name="upload">Upload</button>
-            </div>
+            
           </div>
+
+          <div class="col pt-5 mx-3">
+            <div class="input-group mb-3">
+              <input type="text" class="form-control border-0 border-bottom  rounded-0 bg-transparent house_details" name="description" placeholder="Extra deatil/ Description">
+            </div>
+            <div class="input-group mb-3">
+              <input type="text" class="form-control border-0 border-bottom  rounded-0 bg-transparent house_details" name="price" placeholder="Asking price"required>
+            </div>
+            <div class="input-group mb-3">
+              <input type="text" class="form-control border-0 border-bottom  rounded-0 bg-transparent house_details" name="house_location" placeholder="location"required>
+            </div>
+            <div class="input-group pt-3 mb-2">
+                <label >Upload pictute of the listing</label>
+            </div>
+            <div class="input-group mb-3">
+              <input type="file" class="form-control bg-transparent house_details" name="image" required>
+            </div>
+            <button type="submit" class="btn border bg-transparent submit" name="upload">Upload</button>
+          </div>
+
         </div>
-      </form>
+      </div>
+    </form>
   </main>
 
-<!-- chatbot -->
+  <!-- chatbot -->
   <div class="px-4 my-3" style="position: fixed; right: 0; bottom: 0; z-index: 1030; ">
     <div class="rounded " style="background-color:#ffffffd0">
       <div class="rounded py-3" id="response_area" style="background-color:#ffffffd0">
