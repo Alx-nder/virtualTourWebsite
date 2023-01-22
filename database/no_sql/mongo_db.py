2460ece5447f3d165e1f6fbe59d45f15a5004813
Alx-nder <tyreekalx@gmail.com>
2023-01-21 23:15:34 -0500

    commit to local db

--- database/no_sql/mongo_db.py
+++ database/no_sql/mongo_db.py
@@ -7,3 +7,23 @@ client = MongoClient(cluster_url)
 database= client.virttour
 
 #  a collection is a table
+preference_collection= database.pref
+
+users=[
+    {   "username":"guest",
+        "price_0":"10",
+        "price_1":"9",
+        "price_2":"9",
+        "lucea":"19",
+        "manchester":"6",
+        "kingston":"4",
+        "grange":"5",
+        "mandeville":"5",
+        "mobay":"0"
+    },
+
+    {"username":"hello@hi.hey","price_0":"11","price_1":"7","price_2":"7","lucea":"6","manchester":"1","kingston":"10","grange":"5","mandeville":"1","mobay":"0"},
+    {"username":"t@y.com","price_0":"10","price_1":"1","price_2":"1","lucea":"1","manchester":"9","kingston":"1","grange":"1","mandeville":"0","mobay":"0"}
+]
+
+# preference_collection.insert_many(users)
