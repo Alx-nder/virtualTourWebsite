dc692c82c19f5e1d18f8e3b69368a36731411377
Alx-nder <tyreekalx@gmail.com>
2023-01-26 23:21:25 -0500

    pip mongo

--- server.py
+++ server.py
@@ -1,10 +1,25 @@
-from flask import Flask
+from flask import Flask, Response
+import pymongo
 
 app = Flask(__name__)
+
+try:
+    mongo=pymongo.MongoClient(host="localhost",port=27017,serverSelectionTimeoutMS=1000)
+    mongo.server_info() # trigger exception if cannot connect to db 
+except:
+    print("ERROR - Cannot connect tp db")   
+
+db=mongo.virttour
+
 #######################
 @app.route("/users", methods=["POST"])
 def create_user():
-    return("x")
+    try:
+        user={"name":"first", "lastName":"last"}
+        dbResponse = db.users.insert_one(user)
+
+    except Exception as ex:
+        print(ex)
 
 ##########
 
