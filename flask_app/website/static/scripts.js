7af11554d341b42a2f9df33e8c7dc2abb4334bf5
Alx-nder <tyreekalx@gmail.com>
2023-02-09 23:05:37 -0500

    ajax

---
+++ flask_app/website/static/scripts.js
@@ -0,0 +1,21 @@
+$("form[name=login").submit(function(e){
+    var $form= ($(this));
+    var $error = $form.find(".error");
+    var data=$form.serialize();
+
+    e.preventDefault();
+
+    $.ajax({
+        url: "/register",
+        type: "POST",
+        data: data,
+        dataType: "json",
+        success: function(resp){
+            console.log(resp)
+    },
+        error: function(resp){
+        console.log(resp)  
+        }
+    });
+
+});
