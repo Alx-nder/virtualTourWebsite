8af3d2bc7fb6bee36a38db2c027f5e653456833e
Alx-nder <tyreekalx@gmail.com>
2023-02-12 08:20:38 -0500

    user obj and password hash

--- flask_app/website/models.py
+++ flask_app/website/models.py
@@ -1,5 +1,7 @@
 from flask import Flask,jsonify,request
+from passlib.hash import pbkdf2_sha256
 
+# create the user object
 class User:
     def signup(self):
         user = {
@@ -7,6 +9,8 @@ class User:
             'email':request.form.get('email'),
             'password':request.form.get('password'),
         }
+        # encrypt passord
+        user['password']=pbkdf2_sha256.encrypt(user['password'])
         return jsonify(user)
 
 # https://www.youtube.com/watch?v=mISFEwojJmE
